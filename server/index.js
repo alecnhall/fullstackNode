f4201c56350c1d29433c1cfcb66f561e7b7675d2
Alec Hall <alecnhall@gmail.com>
2020-08-11 12:56:40 -0600

    adding passport and file changes

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const passport = require('passport');
 const app = express();
 
 app.get('/', (req, res) => {
   res.send({ bye: 'guy' });
 });
+
+app.post('/auth/google', (req, res) => {});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
