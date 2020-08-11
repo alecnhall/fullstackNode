7f5ea55a27146451bf6cf0b47c82c845e679959e
Alec Hall <alecnhall@gmail.com>
2020-08-11 13:39:41 -0600

    ready to test oauth

--- server/index.js
+++ server/index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const passport = require('passport');
 const googleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 const app = express();
 
 // implementing passport strategy
 // console.developers.google.com
-passport.use(new googleStrategy());
+passport.use(
+  new googleStrategy(
+    {
+      clientID: keys.googleClientId,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback',
+    },
+    (accessToken) => {
+      console.log(accessToken);
+    }
+  )
+);
 
 // test route
 // app.get('/', (req, res) => {
